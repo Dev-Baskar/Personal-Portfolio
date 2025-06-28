a4c6d26961dba89f9aedeb8f4048d28fe82bffc7
Dev-Baskar <baskar13008@gmail.com>
2025-06-28 23:05:15 +0530

    feat : this is my complete personal portfolio

---
+++ script.js
@@ -0,0 +1,9 @@
+function toggleMenu() {
+    const menu = document.getElementById('mobileMenu');
+    const toggle = document.getElementById('menuToggle');
+    menu.classList.toggle('show');
+    toggle.classList.toggle('open');
+  }
+
+
+  
