3597d2be76dcd43e31c1ecb470748a677f181522
Dev-Baskar <baskar13008@gmail.com>
2025-06-29 17:29:02 +0530

    feat : "adding scroll animation"

--- script.js
+++ script.js
@@ -7,3 +7,51 @@ function toggleMenu() {
 
 
   
+
+  gsap.registerPlugin(ScrollTrigger);
+
+  const sections = ["#about", "#skills", "#projects", "#contact"];
+  sections.forEach((section) => {
+    gsap.from(section, {
+      scrollTrigger: {
+        trigger: section,
+        start: "top 80%",
+        toggleActions: "play none none reset",
+      },
+      opacity: 0,
+      y: 50,
+      duration: 1.2,
+      ease: "power3.out",
+    });
+  });
+
+  gsap.utils.toArray(".tech-item").forEach((el) => {
+    gsap.from(el, {
+      scrollTrigger: {
+        trigger: el,
+        start: "top 90%",
+        toggleActions: "play none none reset",
+      },
+      opacity: 0,
+      y: 30,
+      duration: 0.5,
+      ease: "power2.out",
+    });
+  });
+
+
+  gsap.utils.toArray(".project-card").forEach((card) => {
+    gsap.from(card, {
+      scrollTrigger: {
+        trigger: card,
+        start: "top 85%",
+        toggleActions: "play none none reset",
+      },
+      opacity: 0,
+      scale: 0.95,
+      y: 20,
+      duration: 0.8,
+      ease: "back.out(1.7)",
+    });
+  });
+
